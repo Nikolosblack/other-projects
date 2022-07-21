b93a394a11f92efd10cca1e12b81540fba198790
Nikolos Black <Nikolosblack@gmail.com>
2022-07-21 15:59:57 +0300

    add some changes

--- softc-company/js/burger.js
+++ softc-company/js/burger.js
@@ -1,20 +1,3 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navLinks = document.querySelector(".nav-links__mob");
-// const links = document.querySelectorAll(".nav-links__mob li");
-
-// hamburger.addEventListener('click', ()=>{
-//     document.body.classList.toggle("lock");
-//    //Animate Links
-//     navLinks.classList.toggle("open");
-//     links.forEach(link => {
-//         link.classList.toggle("fade");
-      
-//     });
-//     //Hamburger Animation
-//     hamburger.classList.toggle("toggle");
-// });
-
-
 (function() {
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".nav-links__mob");
