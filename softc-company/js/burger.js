b08b5a3847a481a099a8f05558b84da9d93b56ad
Nikolos Black <Nikolosblack@gmail.com>
2022-07-21 15:03:29 +0300

    change burger.js and add overflow:hidden on click in body

--- softc-company/js/burger.js
+++ softc-company/js/burger.js
@@ -1,15 +1,35 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links__mob");
-const links = document.querySelectorAll(".nav-links__mob li");
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links__mob");
+// const links = document.querySelectorAll(".nav-links__mob li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
+// hamburger.addEventListener('click', ()=>{
+//     document.body.classList.toggle("lock");
+//    //Animate Links
+//     navLinks.classList.toggle("open");
+//     links.forEach(link => {
+//         link.classList.toggle("fade");
+      
+//     });
+//     //Hamburger Animation
+//     hamburger.classList.toggle("toggle");
+// });
+
+
+(function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links__mob");
+    const links = document.querySelectorAll(".nav-links__mob li");
+    const lockbody = document.querySelector("body")
+    hamburger.addEventListener("click", function(e) {
+        // add overflow:hidden to body
+        lockbody.classList.toggle("lock");
+        //Hamburger Animation
+        hamburger.classList.toggle("toggle");
+        //opacity to li - Animate Links
+        navLinks.classList.toggle("open");
+            links.forEach(link => {
+                link.classList.toggle("fade");
+            });
     });
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-
-});
-
+}
+)();
