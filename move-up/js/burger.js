cdb48fbfd429481cf1a617a7194cc8467f91dabc
Nikolos Black <Nikolosblack@gmail.com>
2022-07-25 13:25:52 +0300

    add some changes in burger.js and change css in mobile menu

--- move-up/js/burger.js
+++ move-up/js/burger.js
@@ -1,8 +1,23 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const mobMenu = document.querySelector('.mobile__menu__wrapper')
+(function() {
+	const burgerMenu = document.querySelector('.burger-menu');
+	const mobMenu = document.querySelector('.mobile__menu__wrapper')
 
-burgerMenu.addEventListener('click', function() {
-    this.classList.toggle("close");
-    document.body.classList.toggle("lock");
-    mobMenu.classList.toggle("active")
-});
+	burgerMenu.addEventListener('click', function(e) {
+		this.classList.toggle("close");
+		document.body.classList.toggle("lock");
+		mobMenu.classList.toggle("active")
+	});
+
+
+	const navLinks = document.querySelectorAll(".menu__link");
+	navLinks.forEach((navLink) => {
+		navLink.addEventListener("click", (_) => {
+			if (burgerMenu.classList.contains("close")) {
+				document.body.classList.remove("lock");
+				burgerMenu.classList.remove("close");
+				mobMenu.classList.remove("active");
+			}
+		});
+	});
+
+})();
