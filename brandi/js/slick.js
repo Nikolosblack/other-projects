e6c0e2b050e23a24a590da7f05fdbef1c9b4e003
Nikolos Black <Nikolosblack@gmail.com>
2022-07-27 17:41:38 +0300

    add some changes in css

--- brandi/js/slick.js
+++ brandi/js/slick.js
@@ -3,13 +3,13 @@ $('.responsive').slick({
     dots: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
           dots: false
@@ -18,7 +18,7 @@ $('.responsive').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       },
