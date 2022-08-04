5958618efbbd90952643a555617198e936a93581
Nikolos Black <Nikolosblack@gmail.com>
2022-08-04 19:22:02 +0300

    add tag main, clear html code

--- warehouse/js/burger.js
+++ warehouse/js/burger.js
@@ -1,22 +1,21 @@
-(function() {
-	const burgerMenu = document.querySelector('.nav-icon-8');
-	const mobMenu = document.querySelector('.nav__bar');
+(function () {
+  const burgerMenu = document.querySelector(".nav-icon-8");
+  const mobMenu = document.querySelector(".nav__bar");
 
-	burgerMenu.addEventListener('click', function(e) {
-		this.classList.toggle("open");
-		document.body.classList.toggle("lock");
-		mobMenu.classList.toggle("active");
-	});
+  burgerMenu.addEventListener("click", function (e) {
+    this.classList.toggle("open");
+    document.body.classList.toggle("lock");
+    mobMenu.classList.toggle("active");
+  });
 
-	const navLinks = document.querySelectorAll(".menu__link");
-	navLinks.forEach((navLink) => {
-		navLink.addEventListener("click", (_) => {
-			if (burgerMenu.classList.contains("open")) {
-				document.body.classList.remove("lock");
-				burgerMenu.classList.remove("open");
-				mobMenu.classList.remove("active");
-			}
-		});
-	});
-
+  const navLinks = document.querySelectorAll(".menu__link");
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (_) => {
+      if (burgerMenu.classList.contains("open")) {
+        document.body.classList.remove("lock");
+        burgerMenu.classList.remove("open");
+        mobMenu.classList.remove("active");
+      }
+    });
+  });
 })();
