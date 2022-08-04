3b9c7c21542ccd54035485be769c6f2cb2a7507f
Nikolos Black <Nikolosblack@gmail.com>
2022-08-04 19:20:12 +0300

    add js burger, add some changes in css, formated code

---
+++ warehouse/js/burger.js
@@ -0,0 +1,22 @@
+(function() {
+	const burgerMenu = document.querySelector('.nav-icon-8');
+	const mobMenu = document.querySelector('.nav__bar');
+
+	burgerMenu.addEventListener('click', function(e) {
+		this.classList.toggle("open");
+		document.body.classList.toggle("lock");
+		mobMenu.classList.toggle("active");
+	});
+
+	const navLinks = document.querySelectorAll(".menu__link");
+	navLinks.forEach((navLink) => {
+		navLink.addEventListener("click", (_) => {
+			if (burgerMenu.classList.contains("open")) {
+				document.body.classList.remove("lock");
+				burgerMenu.classList.remove("open");
+				mobMenu.classList.remove("active");
+			}
+		});
+	});
+
+})();
---
+++ warehouse/js/slick.js
@@ -0,0 +1,9 @@
+$('.responsive').slick({
+    infinite: true,
+    dots: true,
+    speed: 300,
+    arrows : false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+});
