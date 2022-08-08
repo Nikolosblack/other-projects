eaf5e54334d2793bbc9936d9853da92e07483ea8
Nikolos Black <Nikolosblack@gmail.com>
2022-08-08 19:50:53 +0300

    add new project cloud-budget, add header and hero sections with css style, add burger with js

---
+++ cloud-budget/js/burger.js
@@ -0,0 +1,23 @@
+
+(function() {
+	const burgerMenu = document.querySelector('.burger-menu');
+	const mobMenu = document.querySelector('.nav__bar');
+	burgerMenu.addEventListener('click', function(e) {
+		this.classList.toggle("close");
+		document.body.classList.toggle("lock");
+		mobMenu.classList.toggle("active");
+
+	});
+
+	const navLinks = document.querySelectorAll(".menu__link");
+	navLinks.forEach((navLink) => {
+		navLink.addEventListener("click", (_) => {
+			if (burgerMenu.classList.contains("close")) {
+				document.body.classList.remove("lock");
+				burgerMenu.classList.remove("close");
+				mobMenu.classList.remove("active");
+			}
+		});
+	});
+
+})();
