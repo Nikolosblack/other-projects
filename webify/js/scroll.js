6d8b6ae39a5da5a45c5c464a786673f97b1e5e65
Nikolos Black <Nikolosblack@gmail.com>
2022-09-01 16:24:56 +0300

    add some changes

--- webify/js/scroll.js
+++ webify/js/scroll.js
@@ -30,3 +30,23 @@ scrollToTopBtn.addEventListener("click", scrollToTop);
 let observer = new IntersectionObserver(callback);
 // Finally start observing the target element
 observer.observe(target);
+
+// animation in section
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 0;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+window.addEventListener("scroll", reveal);
